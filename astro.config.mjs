76d34eb9f2d512417a9d59c0c7ec4728bab97f82
embercleaver <61590140+embercleaver@users.noreply.github.com>
2025-09-16 18:05:46 -0400

    added ui component for greeting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://warm-selkie-9155d2.netlify.app/"
+  site: "https://warm-selkie-9155d2.netlify.app/",
+  integrations: [preact()]
 });
